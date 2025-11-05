1bfcb32090955ff52eb0195b83ee7a62854a7303
AdiKum26 <adideeavi1@gmail.com>
2025-11-05 12:57:46 -0800

    Update deployment config for root domain (AdiKum26.github.io)

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,23 @@ import "./index.css";
 // Handle GitHub Pages routing
 // https://github.com/rafgraph/spa-github-pages
 (function() {
-  const basePath = '/adityakumar.github.io';
   const { pathname, search } = window.location;
-  const pathIsIndex = pathname === basePath + '/index.html' || pathname === '/index.html';
+  const pathIsIndex = pathname === '/index.html';
   const hasQuery = search.includes('?/');
   
   if (pathIsIndex || hasQuery) {
-    let newPathname = basePath + '/';
+    const pathnameToUse = pathIsIndex ? '/index.html' : pathname;
+    const segments = pathnameToUse.split('/').filter(s => s);
+    const restOfPath = segments.length > 1 ? segments.slice(1).join('/') : '';
+    
+    let newPathname = '/';
     if (hasQuery) {
       const match = search.match(/\?\/+(.*)/);
       if (match) {
-        const path = match[1].split('&')[0].replace(/~and~/g, '&');
-        newPathname = basePath + (path.startsWith('/') ? path : '/' + path);
+        newPathname = match[1].split('&')[0].replace(/~and~/g, '&');
+        if (!newPathname.startsWith('/')) {
+          newPathname = '/' + newPathname;
+        }
       }
     }
     
